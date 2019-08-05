a684355f7371f6a61e695414c498b96f67b7c913
mbu93 <skorp42@web.de>
2019-08-05 16:47:28 +0200

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytorch-vis",
+    name="pytorch_vis",
     version="0.0.1",
     author="Utku Ozbulak",
     description="A package to visualize pytorch CNN models",
